6828167559104dfd73aaa9bed9fe860de5c7d836
Amirhosein Karheydari@gmail.com <96719970+Kar0o0@users.noreply.github.com>
2025-02-23 14:19:18 +0330

    CBV - Templates - Tests

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import homepagepiew
+from .views import HomePageView, AboutPageView
 urlpatterns = [
-    path('', homepagepiew, name='home')
+    path('', HomePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about')
 ]
